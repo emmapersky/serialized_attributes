d6001898522f5895d1fda2fb0234b86be0d48392
Nickolay Abdrafikov <nicck.olay@gmail.com>
2010-07-26 22:53:11 +0400

    update test

--- test/simple_test.rb
+++ test/simple_test.rb
@@ -14,12 +14,6 @@ class DocumentsSchema < ActiveRecord::Migration
       t.integer :reference_id           # <---  you can also define any sql columns for your indexes
       t.timestamps
     end
-    
-    create_table :mixeds do |t|
-      t.title
-      t.body
-      t.text :serialized_attributes     # <---  here all your dynamic fields will be saved
-    end
   end
 end
 
@@ -31,36 +25,35 @@ class Post < Document
   attribute :title, String
   attribute :body,  String
   attribute :is_published, Boolean, :default => false
-  
+
   attribute :comment_ids, Array     # <--- serialized Array of ids of associated comments
   has_references_to :comments
-  
+
   validates_presence_of :title, :body
-  
+
 end
 
 class Comment < Document
   attribute :body, String
   attribute :post_id, Integer
   belongs_to :post
-  
+
   validates_presence_of :body
-  
+
 end
 
-class Mixed < ActiveRecord::Base
-  set_table_name :mixeds
+class ModelBefore < ActiveRecord::Base
+  set_table_name :documents
 end
 
-class MixedWithSA < ActiveRecord::Base
-  set_table_name :mixeds
-
+class ModelAfter < ActiveRecord::Base
+  set_table_name :documents
   include SerializedAttributes
   attribute :custom_field, String, :default => 'default value'
 end
 
 class SimpleTest < Test::Unit::TestCase
-  # ActiveRecord::Base.logger = Logger.new(STDOUT)
+  ActiveRecord::Base.logger = Logger.new(STDOUT)
   DocumentsSchema.suppress_messages{ DocumentsSchema.migrate(:up) }
 
   def test_simple
@@ -72,13 +65,12 @@ class SimpleTest < Test::Unit::TestCase
     assert_equal Comment.all.map(&:id), post.comment_ids
     post.save
     assert_equal 3, post.reload.comments.size
-    
   end
-  
-  def test_null_serialized_attributes_column_on_already_exists_records
-    Mixed.create
-    doc = MixedWithSA.first
 
-    assert_equal doc.custom_field, 'default value'
+  def test_null_serialized_attributes_column_on_already_exists_records
+    model_before = ModelBefore.create
+    model_after = ModelAfter.find(model_before.id)
+
+    assert_equal model_after.custom_field, 'default value'
   end
 end
